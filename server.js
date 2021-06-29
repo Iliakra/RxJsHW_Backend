57ff6b3f9d43370f9d5d340de58d70336d511f72
Ilya Krasilnikov <kravus27@gmail.com>
2021-06-29 12:52:48 +0300

    eedede

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const ChatServer = require('./ChatServer');
+
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const Router = require('koa-router');
-const uuid = require('uuid');
-const app= new Koa();
+const app = new Koa();
 const router = new Router();
 
-let users = [];
+const chat = new ChatServer();
+
+// Koa body initialize
+app.use(
+    koaBody({
+      urlencoded: true,
+    })
+  );
 
 app.use(async(ctx, next) => { 
     const origin = ctx.request.get('Origin');
@@ -40,6 +48,76 @@ app.use(async(ctx, next) => {
     }
 });
 
+router.get('/users', async (ctx) => {
+    ctx.response.type = 200;
+    ctx.response.body = chat.getConnectedUsers(); 
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 
 const port = process.env.PORT||7070;
-const server = http.createServer(app.callback()).listen(port);
+const server = http.createServer(app.callback());
+
+const WS = require('ws');
+const wsServer = new WS.Server({ server });
+
+wsServer.on('connection', (ws, req) => {
+    const errCallback = (err) => {
+      if (err) {
+        // TODO: handle error
+        console.log(err);
+      }
+    };
+  
+    ws.on('message', data => {
+      // console.log('msg');
+      let msg;
+      try { msg = JSON.parse(data) }
+      catch (e) { console.log(`Error on ${data}`) }
+      if (!msg) return;
+      if (msg.type === 'register') {
+        ws.send(JSON.stringify({ type: 'register', userID: chat.addUser(msg.userName).id }));
+        Array.from(wsServer.clients)
+            .filter(o => o.readyState === WS.OPEN)
+            .forEach(o => o.send(JSON.stringify({
+              type: 'users',
+              users: chat.getConnectedUsers(),
+            })));
+        return;
+      }
+      if (msg.type === 'getPrevious') {
+        ws.send(JSON.stringify({ type: 'previous', messages: chat.getPreviousMessages(msg.count) }), errCallback);
+        return;
+      }
+      if (msg.type === 'message') {
+        const message = chat.pushMessage(msg.userID, msg.content);
+        if (message) {
+          Array.from(wsServer.clients)
+            .filter(o => o.readyState === WS.OPEN)
+            .forEach(o => o.send(JSON.stringify({
+              message,
+              type: 'message',
+            })));
+        }
+        return;
+      }
+      ws.send(JSON.stringify({ type: 'error', message: 'unknown type of message' }), errCallback);
+    });
+  
+    ws.on('close', data => {
+      chat.removeUser((JSON.parse(data)).userID);
+      Array.from(wsServer.clients)
+            .filter(o => o.readyState === WS.OPEN)
+            .forEach(o => o.send(JSON.stringify({
+              type: 'users',
+              users: chat.getConnectedUsers(),
+            })));
+    });
+});
+
+server.listen(port);
+console.log(`Server is listening on port ${port}`);
+
+  
